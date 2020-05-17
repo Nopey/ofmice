e3ef715240104fc4e40057694f9814c3632bdff6
Magnus Larsen <golgothasTerror101@gmail.com>
2020-05-17 01:25:46 -0700

    Get some GTK Launcher goodness going

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,106 @@
 //! main is the vgtk frontend of the code. Perhaps it should be moved into its own interface module?
 #![deny(clippy::all)]
-#![recursion_limit="1024"]
 mod steam_wrangler;
 mod platform;
 mod download;
 mod installation;
 
-use gtk::*;
-use gdk_pixbuf::Pixbuf;
-use gio::{ApplicationFlags, Cancellable, MemoryInputStream, Icon};
-use glib::Bytes;
 use gtk::prelude::*;
-
-#[derive(Clone, Debug, Default)]
-struct Model {}
-
-#[derive(Clone, Debug)]
-enum Message {
-    Noop,
-    Exit,
-}
+use gio::prelude::*;
+use gdk_pixbuf::Pixbuf;
+use gio::{ApplicationFlags, Cancellable, MemoryInputStream};
+use glib::Bytes;
 
 fn load_bg() -> Pixbuf {
-    static BG: &[u8] = include_bytes!("bg.png");
+    static BG: &[u8] = include_bytes!("res/bg.png");
     let data_stream = MemoryInputStream::new_from_bytes(&Bytes::from_static(BG));
     Pixbuf::new_from_stream(&data_stream, None as Option<&Cancellable>).unwrap()
 }
 
-impl Component for Model {
-    type Message = Message;
-    type Properties = ();
+fn load_logo() -> Pixbuf {
+    static DATA: &[u8] = include_bytes!("res/logo.svg");
+    let data_stream = MemoryInputStream::new_from_bytes(&Bytes::from_static(DATA));
+    Pixbuf::new_from_stream(&data_stream, None as Option<&Cancellable>).unwrap()
+}
 
-    fn update(&mut self, msg: Self::Message) -> UpdateAction<Self> {
-        match msg {
-            // Message::Noop => UpdateAction::None,
-            Message::Exit => {
-                vgtk::quit();
-                UpdateAction::None
-            },
-        }
-    }
+fn load_play_icon() -> Pixbuf {
+    static ICON: &[u8] = include_bytes!("res/play.png");
+    let data_stream = MemoryInputStream::new_from_bytes(&Bytes::from_static(ICON));
+    Pixbuf::new_from_stream(&data_stream, None as Option<&Cancellable>).unwrap()
+}
 
-    fn view(&self) -> VNode<Model> {
-        gtk! {
-            <Application::new_unwrap(Some("fun.openfortress.ofmice"), ApplicationFlags::empty())>
-                <Window app_paintable=true resizable=false on destroy=|_| Message::Exit>
-                    // The Grid puts background behind the stack,
-                    // That's my idea of a good hack.
-                    <Grid>
-                        <Stack>
-                            // <Label label="Hello!" valign=Align::Center halign=Align::Center />
-                            <Image
-                                /*pixbuf=Pixbuf::new_from_resource("document-save").ok()*/
-                                valign=Align::Center halign=Align::Center
-                                // this isn't working
-                                on configure_event=|i, _| {
-                                    i.set_from_gicon(&Icon::new_for_string("process-working").unwrap(), IconSize::LargeToolbar);
-                                    Message::Noop
-                                }
-                            />
-                            <Box orientation=Orientation::Vertical spacing=12 valign=Align::Center>
-                                /* Loading and Status */
-                                <ProgressBar text="Progress Bar" show_text=true hexpand=true/>
-                                <Button label="Start" halign=Align::Center />
-                            </Box>
-                        </Stack>
-                        <Image pixbuf=Some(load_bg()) />
-                    </Grid>
-                </Window>
-            </Application>
-        }
-    }
+fn load_config_icon() -> Pixbuf {
+    static ICON: &[u8] = include_bytes!("res/config.png");
+    let data_stream = MemoryInputStream::new_from_bytes(&Bytes::from_static(ICON));
+    Pixbuf::new_from_stream(&data_stream, None as Option<&Cancellable>).unwrap()
+}
+
+fn load_css() -> &'static [u8] {
+    include_bytes!("res/main.css")
+}
+
+fn load_glade() -> &'static str {
+    include_str!("res/main.glade")
+}
+
+fn build_ui(application: &gtk::Application) {
+    use gtk::*;
+
+    // Build our UI from ze XML
+    let builder = Builder::new_from_string(load_glade());
+
+    // Apply the CSS to ze XML
+    let provider = CssProvider::new();
+    provider
+        .load_from_data(load_css())
+        .expect("Failed to load CSS");
+    StyleContext::add_provider_for_screen(
+        &gdk::Screen::get_default().expect("Error initializing gtk css provider."),
+        &provider,
+        STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
+
+    // window needs application
+    let window: ApplicationWindow = builder.get_object("window").unwrap();
+    window.set_application(Some(application));
+
+    // Set the background image
+    let background: Image = builder.get_object("background").unwrap();
+    background.set_from_pixbuf(Some(&load_bg()));
+
+    // Overlay the stack onto the background
+    let overlay: Overlay = builder.get_object("overlay").unwrap();
+    let stack: Stack = builder.get_object("stack").unwrap();
+    overlay.add_overlay(&stack);
+
+    // Set the tab's icons
+    let play_tabicon: Image = builder.get_object("play-tab").unwrap();
+    play_tabicon.set_from_pixbuf(Some(&load_play_icon()));
+    let config_tabicon: Image = builder.get_object("config-tab").unwrap();
+    config_tabicon.set_from_pixbuf(Some(&load_config_icon()));
+
+    // Set the main logo
+    let logo: Image = builder.get_object("logo").unwrap();
+    logo.set_from_pixbuf(Some(&load_logo()));
+
+    window.show_all();
 }
 
 #[tokio::main]
 async fn main() {
+    // pretty_env_logger::init();
+
     let old = installation::Installation::try_load().unwrap_or_default();
     let mut new = old.clone();
     download::download(&mut new).await.unwrap();
     new.save_changes().unwrap();
-    pretty_env_logger::init();
-    std::process::exit(run::<Model>());
+
+
+    let uiapp = gtk::Application::new(Some("fun.openfortress.ofmice"),
+                    ApplicationFlags::FLAGS_NONE)
+                .expect("Application::new failed");
+
+    uiapp.connect_activate(build_ui);
+
+    uiapp.run(&std::env::args().collect::<Vec<_>>());
 }
